b92f0235d80f616e7e6c2e377364901a351834ba
Behera Manav <manavbehera123@gmail.com>
2025-11-13 21:09:49 +0530

    feat(action): add sortingOrder field to IAction interface and ActionSchema

--- hooks/src/model/Action.ts
+++ hooks/src/model/Action.ts
@@ -5,6 +5,7 @@ export interface IAction {
   AvailableAction: Types.ObjectId;
   event: IEvent;
   zapId: Types.ObjectId;
+  sortingOrder: number;
 }
 
 const ActionSchema = new Schema<IAction>({
@@ -18,6 +19,7 @@ const ActionSchema = new Schema<IAction>({
     eventId: { type: String, required: true },
   },
   zapId: { type: Schema.Types.ObjectId, ref: "Zap", required: true },
+  sortingOrder: { type: Number, required: true },
 });
 
 export const ActionModel = {
